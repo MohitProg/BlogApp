8bce45837d48eb3a7e2df59fd63e063f442dced4
MohitProg <mohitjj999@gmail.com>
2024-03-04 23:08:50 +0530

    Adding navbar

--- src/App.js
+++ src/App.js
@@ -7,6 +7,15 @@ function App() {
   <>
   
 <Header/>
+
+{/* <div class="relative inline-block group">
+  <button class="hover:bg-gray-300 px-4 py-2 rounded-md">Hover Here</button>
+  <ul class="hidden absolute z-10 top-9 left-0 bg-white border border-gray-200 py-2 rounded-md shadow-lg group-hover:block">
+    <li><a href="#" class="block px-4 py-2 hover:bg-gray-100">Item 1</a></li>
+    <li><a href="#" class="block px-4 py-2 hover:bg-gray-100">Item 2</a></li>
+    <li><a href="#" class="block px-4 py-2 hover:bg-gray-100">Item 3</a></li>
+  </ul>
+</div> */}
   </>
   );
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,77 +1,120 @@
-import React from "react";
-
 const Header = () => {
     return (
         <>
-            <header className="boder shadow">
-                <nav className="p-[10px] flex gap-4 items-center container  mx-auto">
-                    <h1 className="font-bold text-2xl"><span className="text-red-600">Web</span>tech</h1>
+            <header className="boder flex  items-center justify-center ">
+                <nav className="p-[10px] flex gap-4 w-3/4  items-center justify-center   mx-auto">
+                    <h1 className="font-bold text-2xl">
+                        <span className="text-red-600">Web</span>tech
+                    </h1>
 
-                    <div className="w-full  px-4 ">
+                    <div className="  px-4 ">
                         <ul className="flex gap-10 items-center">
-
                             <div className="">
-                                <li className="text-sm  hover:text-red-500 font-bold flex items-center">
-                                    HOME<span className="material-symbols-outlined ">expand_more</span>
+                                <li className="text-[11px]  hover:text-red-500 font-bold flex items-center">
+                                    HOME
+                                    <span className=" text-[15px] material-symbols-outlined ">
+                                        expand_more
+                                    </span>
                                 </li>
-
                             </div>
 
-                            <div className="relative  flex items-center justify-center">
-                                <li className="text-sm  hover:text-red-500 font-bold flex items-center justify-center">
-                                    FEATURE<span className="material-symbols-outlined ">expand_more</span>
+                            <div className="  relative flex items-center justify-center group">
+                                <li className="text-[11px]  hover:text-red-500 font-bold flex items-center justify-center">
+                                    FEATURE
+                                    <span className=" text-[15px] material-symbols-outlined ">
+                                        expand_more
+                                    </span>
                                 </li>
+                                {/* feature drop menu */}
 
-                                <div className="absolute top-[60px] border shadow p-3 " >
-
-                                    <ul className="flex-col gap-2">
-
-                                        <li className="text-sm  my-1 flex items-center hover:text-red-500 ">
-                                            FEATURES
-                                            <span className="material-symbols-outlined">expand_more</span>
+                                <ul className="flex-col gap-2  w-[120px]  bg-gray-300  text-[11px] hidden absolute  z-20 top-[15px] group-hover:block  border shadow-lg p-2 ">
+                                    <div className="  group/item ">
+                                        <li className=" text-[11px] p-1 my-1 flex items-center hover:text-red-500 ">
+                                            Multi Dropdown
+                                            <span className="rotate-[-90deg] text-[15px] material-symbols-outlined">
+                                                expand_more
+                                            </span>
                                         </li>
-                                        <li className="text-sm  my-1 flex items-center hover:text-red-500 ">
-                                            FEATURES
 
-                                        </li>
-                                        <li className="text-sm  my-1 flex items-center hover:text-red-500 ">
-                                            FEATURES
-
-                                        </li>
-                                        <li className="text-sm  my-1 flex items-center hover:text-red-500 ">
-                                            FEATURES
-
-                                        </li>
-                                    </ul>
-
-                                </div>
+                                        <ul className="w-[100px] p-2 bg-gray-300  absolute z-50 left-[110px] top-2 hidden group-hover/item:block  shadow-lg">
+                                            <li className=" p-1 my-1 flex items-center hover:text-red-500 ">
+                                                ShortCodes
+                                            </li>
+                                            <li className=" p-1 my-1 flex items-center hover:text-red-500 ">
+                                                FEATURES
+                                            </li>
+                                            <li className=" p-1 my-1 flex items-center hover:text-red-500 ">
+                                                FEATURES
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <li className=" p-1 my-1 flex items-center hover:text-red-500 ">
+                                        ShortCodes
+                                    </li>
+                                    <li className=" p-1 my-1 flex items-center hover:text-red-500 ">
+                                        FEATURES
+                                    </li>
+                                    <li className=" p-1 my-1 flex items-center hover:text-red-500 ">
+                                        FEATURES
+                                    </li>
+                                </ul>
                             </div>
 
+                            <div className="relative group/menu ">
+                                <li className="text-[11px] hover:text-red-500 flex items-center font-bold">
+                                    MEGAMENU
+                                    <span className=" text-[15px] material-symbols-outlined">
+                                        expand_more
+                                    </span>
+                                </li>
 
+                                {/* <div className="absolute text-[11px] bg-gray-300 shadow-lg right-0 left-0  w-[400px]  group-hover/menu:block hidden">
+                  <div>div1</div>
+                  <div>div2</div>
+                  <div>div3</div>
+                </div> */}
+                            </div>
 
+                            <div className="relative  group/doc ">
+                                <li className="text-[11px] hover:text-red-500  flex items-center font-bold">
+                                    DOCUMENTAION
+                                    <span className=" text-[15px] material-symbols-outlined">
+                                        expand_more
+                                    </span>
+                                </li>
 
-
-
-
-                            <li className="text-sm hover:text-red-500 flex items-center font-bold">
-                                MEGAMENU
-                                <span className="material-symbols-outlined">expand_more</span>
-                            </li>
-
-
-
-                            <li className="text-sm hover:text-red-500  flex items-center font-bold">
-                                DOCUMENTAION
-                                <span className="material-symbols-outlined">expand_more</span>
-                            </li>
-
-
-
+                                <ul className="w-[100px] p-2 bg-gray-300 absolute z-50  top-4 hidden group-hover/doc:block  shadow-lg">
+                                    <li className=" p-1 my-1 text-[11px] flex items-center hover:text-red-500 ">
+                                        webdoc
+                                    </li>
+                                    <li className=" p-1 my-1 text-[11px] flex items-center hover:text-red-500 ">
+                                        vidodoc
+                                    </li>
+                                </ul>
+                            </div>
                         </ul>
                     </div>
 
-                    <span className="material-symbols-outlined text-lg hover:text-red-500">search</span>
                 </nav>
+                <span className="material-symbols-outlined  text-lg hover:text-red-500">
+                    search
+                </span>
+            </header>
+
+            <header className="bg-red-500 text-white">
+                <div className="flex container items-center mx-auto justify-between p-1 ">
+                    <ul className="flex items-center gap-3 text-[11px]">
+                        <li>Home</li>
+                        <li>Contact Us</li>
+                        <li>About Us</li>
+                    </ul>
+                    <ul className="flex items-center gap-3 text-[11px]">
+                        <i className="bi bi-instagram"></i>
+                        <i className="bi bi-facebook"></i>
+                        <i className="bi bi-twitter"></i>
+                        <i className="bi bi-linkedin"></i>
+                    </ul>
+                </div>
             </header>
         </>
     );
